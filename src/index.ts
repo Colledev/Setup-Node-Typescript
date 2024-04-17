d3036145e2314fdd60ea8bcf3d68930e7687d51e
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-04-17 18:48:31 -0300

    setup config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world!");
+import express, { Request, Response } from 'express';
+
+const PORT = 5000;
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
